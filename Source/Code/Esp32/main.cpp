0b9dd0764b4d8590bc6b746bc5b159d5229dffc1
shrimp <deltashrimp@proton.me>
2025-03-25 10:54:05 +0500

    pre release

--- Source/Code/Esp32/main.cpp
+++ Source/Code/Esp32/main.cpp
@@ -1,90 +1,134 @@
-// Это код, который надо загрузить на eps32!! Не на esp32-cam!!
-#include <Arduino.h> // Эти библиотеки нужны для работы кода
 #include <WiFi.h>
 #include <WebServer.h>
 
-const char *ssid = "ESP32-AP"; // Имя точки доступа, ip - 192.168.4.1 (ввести в строку поиска в браузере)
-const char *password = "12345678"; // Пароль точки доступа
+// Определяем пины для управления моторами
+#define IN1 26  // Подключен к "1 IN" (MOTOR-A: M1 и M3)
+#define IN2 27  // Подключен к "2 IN" (MOTOR-A: M1 и M3)
+#define IN3 14  // Подключен к "3 IN" (MOTOR-B: M2 и M4)
+#define IN4 12  // Подключен к "4 IN" (MOTOR-B: M2 и M4)
 
+// Учетные данные Wi-Fi
+const char* ssid = "RobotAP"; // Имя сети
+const char* password = "password"; // Пароль
+
+// Создаем веб-сервер на порту 80
 WebServer server(80);
 
-// MX1508 Motor Driver Pins (Пины подключения драйвера к esp32)
-const int motor1Pin1 = 5; // IN1
-const int motor1Pin2 = 18; // IN2
-const int motor2Pin1 = 19; // IN3
-const int motor2Pin2 = 21; // IN4
+// HTML-код веб-страницы
+const char* html = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+<title>Управление роботом</title>
+</head>
+<body>
+<h1>Управление роботом</h1>
+<div style="text-align: center;">
+<button onclick="fetch('/forward')">↑</button><br>
+<button onclick="fetch('/left')">←</button>
+<button onclick="fetch('/stop')">Стоп</button>
+<button onclick="fetch('/right')">→</button><br>
+<button onclick="fetch('/backward')">↓</button>
+</div>
+</body>
+</html>
+)rawliteral";
 
+// Функции управления моторами
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+// Обработчики запросов с веб-страницы
 void handleRoot() {
-    String html = "<html><body><h1>Motor Control</h1>"
-                  "<form action=\"/motor1\" method=\"POST\">"
-                  "<input type=\"submit\" name=\"action\" value=\"Forward\">"
-                  "<input type=\"submit\" name=\"action\" value=\"Backward\">"
-                  "<input type=\"submit\" name=\"action\" value=\"Stop\">"
-                  "</form>"
-                  "<form action=\"/motor2\" method=\"POST\">"
-                  "<input type=\"submit\" name=\"action\" value=\"Forward\">"
-                  "<input type=\"submit\" name=\"action\" value=\"Backward\">"
-                  "<input type=\"submit\" name=\"action\" value=\"Stop\">"
-                  "</form>"
-                  "</body></html>";
-    server.send(200, "text/html", html);
+  server.send(200, "text/html", html);
 }
 
-void handleMotor1() {
-    String action = server.arg("action");
-    if (action == "Forward") {
-        digitalWrite(motor1Pin1, HIGH);
-        digitalWrite(motor1Pin2, LOW);
-    } else if (action == "Backward") {
-        digitalWrite(motor1Pin1, LOW);
-        digitalWrite(motor1Pin2, HIGH);
-    } else {
-        digitalWrite(motor1Pin1, LOW);
-        digitalWrite(motor1Pin2, LOW);
-    }
-    server.sendHeader("Location", "/");
-    server.send(303);
+void handleForward() {
+  moveForward();
+  server.send(200, "text/plain", "OK");
 }
 
-void handleMotor2() {
-    String action = server.arg("action");
-    if (action == "Forward") {
-        digitalWrite(motor2Pin1, HIGH);
-        digitalWrite(motor2Pin2, LOW);
-    } else if (action == "Backward") {
-        digitalWrite(motor2Pin1, LOW);
-        digitalWrite(motor2Pin2, HIGH);
-    } else {
-        digitalWrite(motor2Pin1, LOW);
-        digitalWrite(motor2Pin2, LOW);
-    }
-    server.sendHeader("Location", "/");
-    server.send(303);
+void handleBackward() {
+  moveBackward();
+  server.send(200, "text/plain", "OK");
+}
+
+void handleLeft() {
+  turnLeft();
+  server.send(200, "text/plain", "OK");
+}
+
+void handleRight() {
+  turnRight();
+  server.send(200, "text/plain", "OK");
+}
+
+void handleStop() {
+  stopMotors();
+  server.send(200, "text/plain", "OK");
 }
 
 void setup() {
-    // Initialize serial communication
-    Serial.begin(115200);
-
-    // Initialize motor control pins
-    pinMode(motor1Pin1, OUTPUT);
-    pinMode(motor1Pin2, OUTPUT);
-    pinMode(motor2Pin1, OUTPUT);
-    pinMode(motor2Pin2, OUTPUT);
-
-    // Initialize Wi-Fi access point
-    WiFi.softAP(ssid, password);
-    Serial.println();
-    Serial.print("IP address: ");
-    Serial.println(WiFi.softAPIP());
-
-    // Initialize web server
-    server.on("/", handleRoot);
-    server.on("/motor1", HTTP_POST, handleMotor1);
-    server.on("/motor2", HTTP_POST, handleMotor2);
-    server.begin();
+  // Запускаем Serial для отладки
+  Serial.begin(115200);
+  
+  // Настраиваем пины как выходы
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  
+  // Запускаем точку доступа Wi-Fi
+  WiFi.softAP(ssid, password);
+  Serial.println("Точка доступа запущена");
+  Serial.print("IP-адрес: ");
+  Serial.println(WiFi.softAPIP());
+  
+  // Определяем маршруты веб-сервера
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/forward", HTTP_GET, handleForward);
+  server.on("/backward", HTTP_GET, handleBackward);
+  server.on("/left", HTTP_GET, handleLeft);
+  server.on("/right", HTTP_GET, handleRight);
+  server.on("/stop", HTTP_GET, handleStop);
+  
+  // Запускаем сервер
+  server.begin();
 }
 
 void loop() {
-    server.handleClient();
+  // Обрабатываем запросы клиентов
+  server.handleClient();
 }
